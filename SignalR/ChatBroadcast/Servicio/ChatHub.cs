a7e621b31b3af643ec081de338ce701a34b21103
berczeck_400@hotmail.com <berczeck69@gmail.com>
2014-04-04 01:00:49 -0500

    proyecto inicial de chat

---
+++ SignalR/ChatBroadcast/Servicio/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Servicio
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
