71898d30bb1f56fd7c9c1c7a81aacf5ffaecf1e4
Alexander Aldazabal <aaldazabal@clouddev.epiqcorp.com>
2016-06-05 21:21:21 -0500

    first commit

---
+++ Log4NetLogstashEastic/Log4NetToRabbitMQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Log4NetToRabbitMQ
+{
+    class Program
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            log.Debug("Log Debug");
+            log.Info("Log Info");
+            log.Warn("Log Warn");
+            log.Error("Log Error");
+            log.Fatal("Log Fatal");
+
+            try
+            {
+                string u = null;
+                u.Substring(0);
+            }
+            catch (Exception ex)
+            {
+                //log.Error(ex);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
